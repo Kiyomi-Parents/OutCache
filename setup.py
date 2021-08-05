9c5a65cecefea83b43224c206beb5c78d1d7b41c
LuCkEr <lucker@lucker.xyz>
2021-08-05 10:06:01 +0300

    Fixed version

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r", encoding="utf-8") as fh:
 
 setuptools.setup(
     name="OutCache",
-    version="1.0.5",
+    version="0.0.1",
     author="LuCkEr-",
     author_email="lucker@lucker.xyz",
     description="Function output cacher",
