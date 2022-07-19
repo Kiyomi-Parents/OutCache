aaa1afb740957b8029a7986108a992a641be4106
LuCkEr <lucker@lucker.xyz>
2022-07-19 03:58:07 +0300

    Bump version

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r", encoding="utf-8") as fh:
 
 setuptools.setup(
     name="OutCache",
-    version="1.0.0",
+    version="1.0.1",
     author="LuCkEr-",
     author_email="lucker@lucker.xyz",
     description="Function output cacher",
